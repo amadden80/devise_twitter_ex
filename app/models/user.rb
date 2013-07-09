22eed7189d0c0bad5485849b3e260c1e3d02439f
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-09 14:19:42 -0400

    Adding the magic method for User that either gives them a new Twitter identity or finds them based on Twitter identity

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,35 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 
   has_many :identities
+
+  def self.find_for_twitter_oauth(auth, user)
+    identity = user.identities.where(provider: auth.provider).first
+    if identity
+      identity.update_attributes(
+        name: auth.extra.raw_info.name,
+        user_name: auth.extra.raw_info.screen_name,
+        provider: auth.provider,
+        uid: auth.uid,
+        url: auth.info.urls.Twitter,
+        image: auth.extra.raw_info.profile_image_url_https,
+        token: auth.extra.access_token.token,
+        secret: auth.extra.access_token.secret
+      )
+    else
+      user.identities << Identity.create(
+        name: auth.extra.raw_info.name,
+        user_name: auth.extra.raw_info.screen_name,
+        provider: auth.provider,
+        uid: auth.uid,
+        url: auth.info.urls.Twitter,
+        image: auth.extra.raw_info.profile_image_url_https,
+        token: auth.extra.access_token.token,
+        secret: auth.extra.access_token.secret,
+        user_id: user.id
+      )
+      user.save!
+    end
+
+    user
+  end
 end
