e87baf349768fbe1e081705a6d5b7a4ecc5821b8
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-09 12:06:21 -0400

    Initializing Postgres repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseTwitterEx::Application.config.secret_token = '8857be3b9a724086359d2e0e823617865a522067114e8f4c2be83d95f93ec4777bb4ca407218eec7b3d6c568d0e6444f14e496e089c40f2607c9b3cfe37924bd'
